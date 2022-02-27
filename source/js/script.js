a0c85dfe0e5bf0fddcc310def98ae63684970f5a
ILIDAN <mrster03@gmail.com>
2022-02-28 00:06:46 +0300

    Правки

--- source/js/script.js
+++ source/js/script.js
@@ -1,19 +1,24 @@
 'use strict';
+let body = document.querySelector('.page-body')
 let popup = document.querySelector('.popup');
 let popupOpenButton = document.querySelector('.button');
 let popupCloseButton = document.querySelector('.popup-close');
 let nameInput = document.getElementById("nameInput");
 let phoneInput = document.getElementById("phoneInput");
+let name =document.getElementById('name');
+let phone =document.getElementById('phone');
 
 popup.classList.remove('popup--opened');
 
 popupOpenButton.addEventListener('click', () => {
   popup.classList.add('popup--opened');
+  body.classList.add('page-body--fixed');
   nameInput.focus();
 });
 
 popupCloseButton.addEventListener('click', () => {
   popup.classList.remove('popup--opened');
+  body.classList.remove('page-body--fixed');
 });
 
 window.addEventListener('keydown', (evt) => {
